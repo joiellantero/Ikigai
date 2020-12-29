c7c6c2f9dd3be750201dd504d878c1479c8ac59d
jllantero <jllantero@gmail.com>
2020-12-29 10:21:13 +0800

    fix button bug
    add test.jsx original file

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Page1 from "./Page1";
 import Page2 from "./Page2";
 import Page3 from "./Page3";
-import Page4 from "./test_original";
+import Page4 from "./test";
 import Page5 from "./testcircle";
 
 const rootElement = document.getElementById("root");
