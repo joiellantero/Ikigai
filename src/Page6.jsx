e1720415bf012324dfa7841dfc49cdfa62d5066e
Bea Rosari Supanga <bearosari@gmail.com>
2020-12-29 22:49:09 +0800

    export working on class component
    but buggy

--- src/Page6.jsx
+++ src/Page6.jsx
@@ -1,28 +1,28 @@
 import React, { useRef } from 'react';
 import { useReactToPrint } from 'react-to-print';
 import { render } from "react-dom";
-import Far from "./Page5.jsx";
+import Far from "./Page5test.jsx";
 
-class ComponentToPrint extends React.PureComponent {
-    render() {
-        return (
-            <table>
-                <thead>
-                    <th>column 1</th>
-                    <th>column 2</th>
-                    <th>column 3</th>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>data 1</td>
-                        <td>data 2</td>
-                        <td>data 3</td>
-                    </tr>
-                </tbody>
-            </table>
-        );
-    }
-}
+// class ComponentToPrint extends React.PureComponent {
+//     render() {
+//         return (
+//             <table>
+//                 <thead>
+//                     <th>column 1</th>
+//                     <th>column 2</th>
+//                     <th>column 3</th>
+//                 </thead>
+//                 <tbody>
+//                     <tr>
+//                         <td>data 1</td>
+//                         <td>data 2</td>
+//                         <td>data 3</td>
+//                     </tr>
+//                 </tbody>
+//             </table>
+//         );
+//     }
+// }
 
 const Export = () => {
     const componentRef = useRef();
@@ -32,7 +32,7 @@ const Export = () => {
 
     return (
         <div>
-            <ComponentToPrint ref={componentRef} />
+            <Far ref={componentRef} />
             <button onClick={handlePrint}>Print this out!</button>
         </div>
     );
