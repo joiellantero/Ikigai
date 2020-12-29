d44d637e736dc94305800b5093e2ad7ff9321eaa
Bea Rosari Supanga <bearosari@gmail.com>
2020-12-29 20:58:46 +0800

    aligned page 3 rows

--- src/index.js
+++ src/index.js
@@ -6,13 +6,8 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Page1 from "./Page1";
 import Page2 from "./Page2";
 import Page3 from "./Page3";
-<<<<<<< HEAD
-import Page4 from "./test_original";
-import Page5 from "./testcircle";
-=======
 import Page4 from "./test";
 import Page5 from "./Page5";
->>>>>>> bea
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
