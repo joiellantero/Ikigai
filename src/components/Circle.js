75d0cb103dfc5d43b83d7963e70f9f8d27524b17
jllantero <jllantero@gmail.com>
2020-12-30 11:47:18 +0800

    added ikigai venn diagram

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -1,12 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Form, OverlayTrigger, Tooltip} from 'react-bootstrap';
+import {EditText } from 'react-edit-text';
+import * as d3 from "d3";
+import * as venn from "venn.js";
 import { v4 } from 'uuid';
 import Trash from '../components/trash.js';
 import Info from '../components/info.js';
 
 const initialNotes = []
 
-const Circle = (props)=> {
+const Circle = (props) => {
     const [notes, setNote] = React.useState(initialNotes);
     const [text, setText] = React.useState('');
 
@@ -26,6 +29,13 @@ const Circle = (props)=> {
         setText('');
     }
 
+    const handleEdit = ({name, value}) => {
+        for (let i = 0; i < notes.length; i++){
+            if (name === notes[i].id){
+                notes[i].intext = value;
+            }
+        }
+    }
 
     function Note(props) {
         return (
@@ -38,6 +48,14 @@ const Circle = (props)=> {
             <span className="intext">
                 {props.intext}
             </span>
+
+            <EditText
+                name={props.id}
+                className="edit-text"
+                value={props.intext}
+                onSave={handleEdit}
+            />
+
             {isShown && (
                 <span className="btn-delete-container" onClick={() => props.deleteNote(props.id)}>
                     <Trash />
@@ -64,10 +82,44 @@ const Circle = (props)=> {
         setText('');
         }
     }
+
+    const [sets, setSets] = useState([
+        { sets: ["L"], size: 1000, label: "love" },
+        { sets: ["G"], size: 1000, label: "good" },
+        { sets: ["N"], size: 1000, label: "needs" },
+        { sets: ["P"], size: 1000, label: "paid" },
+        { sets: ["N", "P"], size: 300, label: "np" },
+        { sets: ["N", "G"], size: 300, label: "ng" },
+        { sets: ["L", "G"], size: 300, label: "lg" },
+        { sets: ["L", "P"], size: 300, label: "lp" },
+        { sets: ["N", "P", "L"], size: 300, label: "npl" },
+        { sets: ["N", "L", "G"], size: 300, label: "nlg" },
+        { sets: ["L", "G", "P"], size: 300, label: "lgp" },
+        { sets: ["N", "G", "P"], size: 300, label: "ngp" },
+        { sets: ["N", "G", "P", "L"], size: 80, label: "Ikigai" }
+    ]);
+
+    useEffect(() => {
+        var colors = ['green', 'red', '#293972', '#E5C907'];
+
+        let chart = venn.VennDiagram();
+        d3.select("#venn").datum(sets).call(chart);
+
+        d3.selectAll("#venn .venn-circle path")
+        .style("fill-opacity", 0)
+        .style("stroke-width", 5)
+        .style("stroke-opacity", .5)
+        .style("stroke", function(d,i) { return colors[i]; });
+
+        d3.selectAll("#venn .venn-circle text")
+        .style("fill", function(d,i) { return colors[i]})
+        .style("font-size", "24px")
+        .style("font-weight", "100");
+    }, [sets]);
     
     return ( 
         <>
-            <div className={props.id}>
+            {/* <div className={props.id}>
                 <div className="circle-container" style={{border: props.border}}>
                     <span className="title-container" style={{color: props.headingColor}}>
                         {props.heading1}<br></br><strong>{props.heading2}</strong>
@@ -83,9 +135,10 @@ const Circle = (props)=> {
                         </Container>
                     </div>
                 </div>
-            </div>
+            </div> */}
+            <div id="venn" style={{ textAlign: "center" }}></div>
         </>
     );
 }
- 
-export default Circle; 
+
+export default Circle;
