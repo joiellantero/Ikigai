f30a5a03419e046ff90412f8553ff9279943fcc3
jllantero <jllantero@gmail.com>
2021-01-04 13:17:45 +0800

    added back button at page 2

--- src/Page2.js
+++ src/Page2.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Button } from 'react-bootstrap';
+
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './images/logo.png';
@@ -11,6 +13,19 @@ function Page2() {
     return (
         <>
             <div className="background-2">
+                <div className="btn-back">
+                    <Link to="/">
+                        <Button variant="light">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-left" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#1A1A1A" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <line x1="5" y1="12" x2="19" y2="12" />
+                                <line x1="5" y1="12" x2="11" y2="18" />
+                                <line x1="5" y1="12" x2="11" y2="6" />
+                            </svg>
+                        </Button>
+                    </Link>
+                </div>
+
                 <div className="main-logo">
                     <img src={logo} alt="cs-logo" />
                 </div>
