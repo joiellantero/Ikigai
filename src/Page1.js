f2bc5f6101f82d6328c02a4ea3e542beb2e97ca4
jllantero <jllantero@gmail.com>
2021-01-05 13:41:30 +0800

    fixed image layout in landing page

--- src/Page1.js
+++ src/Page1.js
@@ -2,14 +2,26 @@ import React from "react";
 import { Link } from "react-router-dom";
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import centerLogo from './images/ikigai-logo.png';
+import cornerLogo from './images/csjournaling.png';
 
 function Page1() {
 
     return (
-        <div className="background-1">
-            <Link to="/intro">
+        <div className="page-container-1">
+            {/* <Link to="/intro">
                 <button type="button" class="btn-default btn-lg">Next</button>
-            </Link>
+            </Link> */}
+
+            <div className="corner-logo">
+                <img src={cornerLogo} alt="#CSJournaling" />
+            </div>
+
+            <div className="center-logo">
+                <img src={centerLogo} alt="finding-your-ikigai" />
+            </div>
+
+            <footer></footer>
         </div>
     );
 
