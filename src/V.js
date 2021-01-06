e71294b7fd5fdd514ea662ca2c2bc28663e13943
jllantero <jllantero@gmail.com>
2021-01-06 17:56:03 +0800

    added draggable at V.js

--- src/V.js
+++ src/V.js
@@ -3,19 +3,24 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css"
 
 import React, { useState } from 'react';
+import Draggable from 'react-draggable';
 
 const intext = ['hello', 'how', 'is', 'this'];
 
 
 const V = () => {
     return (
-        <div className="circle25-container">
-            <div className="rounded-circle">
-                <span>
-                    {intext[0]}
-                </span>
+        <>
+            <div className="circle25-container">
+                <Draggable>
+                    <div className="put-border pill-for-circle">
+                        <span>
+                            {intext[0]}
+                        </span>
+                    </div>
+                </Draggable>
             </div>
-        </div>
+        </>
     );
 };
 
