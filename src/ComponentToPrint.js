3c9547945d0652b2cc23c301e60e2d8cd1b0dbaf
nkha0010 <nasir.khalid.new@gmail.com>
2021-01-15 11:19:29 +0500

    passed empty object on reload

--- src/ComponentToPrint.js
+++ src/ComponentToPrint.js
@@ -5,17 +5,112 @@ import Venn from './Venn';
 import {DragDropContext} from 'react-beautiful-dnd';
 import { Row, Modal } from 'react-bootstrap';
 import BackButton from './components/BackButton';
+import { v4 } from 'uuid';
 
 
 const Intermediate  = () => {
-  const { columns, filtered, setColumn, onDragEnd }  = useLocation();
-
+  let {columns} = useLocation();
+  const { setColumn, onDragEnd }  = useLocation();
+	if (!columns){
+		columns = {
+            [v4()]: {
+                id: 'r1',
+                name: 'what you can be PAID FOR',
+                items: [],
+                top: '118px',
+                left: '254px',
+                width: '283px',
+                maxWidth: '283px',
+                height: '82px',
+            },
+            [v4()]: {
+                id: 'r2',
+                name: 'what the WORLD NEEDS',
+                items: [],
+                top: '292px',
+                left: '46px',
+                width: '90px',
+                maxWidth: '150px',
+                height: '258px'
+            },
+            [v4()]: {
+                id: 'r3',
+                name: 'what you LOVE',
+                items: [],
+                top: '642px',
+                left: '259px',
+                width: '271px',
+                maxWidth: '283px',
+                height: '89px'
+            },
+            [v4()]: {
+                id: 'r4',
+                name: 'what you are GOOD AT',
+                items: [],
+                top: '291px',
+                left: '616px',
+                width: '88px',
+                maxWidth: '150px',
+                height: '261px'
+            },
+            [v4()]: {
+                id: 'r5',
+                name: '', // blue yellow
+                items: [],
+                top: '223px',
+                left: '199px',
+                width: '128px',
+                maxWidth: '150px',
+                height: '134px'
+            },
+            [v4()]: {
+                id: 'r6',
+                name: '', // green blue
+                items: [],
+                top: '490px',
+                left: '198px',
+                width: '129px',
+                maxWidth: '150px',
+                height: '130px'
+            },
+            [v4()]: {
+                id: 'r7',
+                name: '', // green red
+                items: [],
+                top: '497px',
+                left: '461px',
+                width: '134px',
+                maxWidth: '150px',
+                height: '128px'
+            },
+            [v4()]: {
+                id: 'r8',
+                name: '', // center
+                items: [],
+                top: '362px',
+                left: '325px',
+                width: '119px',
+                maxWidth: '150px',
+                height: '125px'
+            },
+            [v4()]: {
+                id: 'r9',
+                name: '', // red yellow
+                items: [],
+                top: '230px',
+                left: '460px',
+                width: '132px',
+                maxWidth: '150px',
+                height: '127px'
+            },
+        };
+	}
 
   return (
      <div className="venn-diagram" style={{ display: 'table', margin: '0 auto' }}>
         <DragDropContext onDragEnd={result => onDragEnd(result, columns, setColumn)}>
             <Row className="row-container">
-                <Venn filtered = {filtered} columns ={columns} setColumn = {setColumn}/>
+                <Venn filtered = {columns} columns ={columns} setColumn = {setColumn}/>
             </Row>
         </DragDropContext>
      </div>
@@ -67,7 +162,6 @@ class Print extends Component {
                   <Link
                       to={{
                           pathname: "/lets-find-out-ikigai",
-                          // cols: columns
                       }}
                   >
                       <button className="btn-secondary btn-lg">
@@ -82,7 +176,7 @@ class Print extends Component {
                   pathname: "/u",
               }}
             >
-              <BackButton onClick={this.handleShow}/>
+              <BackButton/>
             </Link>
           </div>
           <ComponentToPrint ref={(el) => (this.componentRef = el)} />
--- src/u.js
+++ src/u.js
@@ -11,13 +11,13 @@ import AddActivity from './AddActivity';
 import Venn from './Venn';
 
 const Circa = () => {
-    const {cols, pathname}  = useLocation();
+    const { cols }  = useLocation();
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
     let circleData = null;
     
-    if (pathname === '/u'){
+    if (cols){
         const rectangleData = [[], [], [], []]
         let i = 0
 
@@ -129,7 +129,107 @@ const Circa = () => {
         };
     }
     else {
-        circleData = cols;
+        circleData = {
+            [v4()]: {
+                id: 'r1',
+                name: 'what you can be PAID FOR',
+                items: [],
+                top: '118px',
+                left: '254px',
+                width: '283px',
+                maxWidth: '283px',
+                height: '82px',
+            },
+            [v4()]: {
+                id: 'r2',
+                name: 'what the WORLD NEEDS',
+                items: [],
+                top: '292px',
+                left: '46px',
+                width: '90px',
+                maxWidth: '150px',
+                height: '258px'
+            },
+            [v4()]: {
+                id: 'r3',
+                name: 'what you LOVE',
+                items: [],
+                top: '642px',
+                left: '259px',
+                width: '271px',
+                maxWidth: '283px',
+                height: '89px'
+            },
+            [v4()]: {
+                id: 'r4',
+                name: 'what you are GOOD AT',
+                items: [],
+                top: '291px',
+                left: '616px',
+                width: '88px',
+                maxWidth: '150px',
+                height: '261px'
+            },
+            [v4()]: {
+                id: 'r5',
+                name: '', // blue yellow
+                items: [],
+                top: '223px',
+                left: '199px',
+                width: '128px',
+                maxWidth: '150px',
+                height: '134px'
+            },
+            [v4()]: {
+                id: 'r6',
+                name: '', // green blue
+                items: [],
+                top: '490px',
+                left: '198px',
+                width: '129px',
+                maxWidth: '150px',
+                height: '130px'
+            },
+            [v4()]: {
+                id: 'r7',
+                name: '', // green red
+                items: [],
+                top: '497px',
+                left: '461px',
+                width: '134px',
+                maxWidth: '150px',
+                height: '128px'
+            },
+            [v4()]: {
+                id: 'r8',
+                name: '', // center
+                items: [],
+                top: '362px',
+                left: '325px',
+                width: '119px',
+                maxWidth: '150px',
+                height: '125px'
+            },
+            [v4()]: {
+                id: 'r9',
+                name: '', // red yellow
+                items: [],
+                top: '230px',
+                left: '460px',
+                width: '132px',
+                maxWidth: '150px',
+                height: '127px'
+            },
+            ['add']: {
+                id: 'r10',
+                name: '', // add activity
+                items: [],
+                top: '',
+                left: '',
+                width: '',
+                height: ''
+            },
+        };
     }
 
     const [columns, setColumn] = useState(circleData);
