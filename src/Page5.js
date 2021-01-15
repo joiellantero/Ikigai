6f508d9ac209050f72b5a7978c3989cb3f926cc3
Bea Rosari Supanga <bearosari@gmail.com>
2021-01-15 13:47:37 +0800

    working share to socials button as post

--- src/Page5.js
+++ src/Page5.js
@@ -1,39 +1,45 @@
 import React, { Component } from "react";
 import ReactToPrint from "react-to-print";
 import { useLocation, Link } from "react-router-dom";
-import {DragDropContext} from 'react-beautiful-dnd';
+import { DragDropContext } from 'react-beautiful-dnd';
 
 import { Col, Row, Modal, InputGroup, FormControl, Button } from 'react-bootstrap';
 
 import Venn from './Venn';
 import BackButton from './components/BackButton';
-import Twitter from './components/Twitter';
-import Facebook from './components/Facebook';
-import Linkedin from './components/Linkedin';
-import Whatsapp from './components/Whatsapp';
 import Trash from './components/trash';
 import Add from './components/Add';
+import {
+    FacebookShareButton,
+    FacebookIcon,
+    LinkedinShareButton,
+    LinkedinIcon,
+    TwitterShareButton,
+    TwitterIcon,
+    WhatsappShareButton,
+    WhatsappIcon,
+} from "react-share";
 
 
-const Intermediate  = () => {
-  const { columns, filtered, setColumn, onDragEnd }  = useLocation();
+const Intermediate = () => {
+    const { columns, filtered, setColumn, onDragEnd } = useLocation();
 
 
-  return (
-     <div className="venn-diagram" style={{ display: 'table', margin: '0 auto' }}>
-        <DragDropContext onDragEnd={result => onDragEnd(result, columns, setColumn)}>
-            <Row className="row-container">
-                <Venn filtered = {filtered} columns ={columns} setColumn = {setColumn}/>
-            </Row>
-        </DragDropContext>
-     </div>
+    return (
+        <div className="venn-diagram" style={{ display: 'table', margin: '0 auto' }}>
+            <DragDropContext onDragEnd={result => onDragEnd(result, columns, setColumn)}>
+                <Row className="row-container">
+                    <Venn filtered={filtered} columns={columns} setColumn={setColumn} />
+                </Row>
+            </DragDropContext>
+        </div>
     );
 }
 
 class ComponentToPrint extends Component {
     render() {
-      return (
-        <Intermediate />
+        return (
+            <Intermediate />
         );
     }
 }
@@ -81,113 +87,141 @@ class Print extends Component {
     }
 
     render() {
-      return (
-        <>
-            <Modal show={this.show} onHide={this.handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Are you sure?</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>Going back to the previous page will erase your progress? Do you want to begin from scratch?</Modal.Body>
-                <Modal.Footer>
-                    <button className="btn-default btn-lg" onClick={this.handleClose}>
-                        No
+        return (
+            <>
+                <Modal show={this.show} onHide={this.handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Are you sure?</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Going back to the previous page will erase your progress? Do you want to begin from scratch?</Modal.Body>
+                    <Modal.Footer>
+                        <button className="btn-default btn-lg" onClick={this.handleClose}>
+                            No
                     </button>
-                    <Link
-                        to={{
-                            pathname: "/lets-find-out-ikigai",
-                            // cols: columns
-                        }}
-                    >
-                        <button className="btn-secondary btn-lg">
-                            Yes
+                        <Link
+                            to={{
+                                pathname: "/lets-find-out-ikigai",
+                                // cols: columns
+                            }}
+                        >
+                            <button className="btn-secondary btn-lg">
+                                Yes
                         </button>
-                    </Link>
-                </Modal.Footer>
-            </Modal>
-            <div className="page-container-6 container">
-                <div className="btn-back">
-                    <Link
-                    to={{
-                        pathname: "/u",
-                    }}
-                    >
-                    <BackButton onClick={this.handleShow}/>
-                    </Link>
-                </div>
-                <p className="subtitle">Your Ikigai, Visualised</p>
-                <div className="row my-5">
-                    <div className="col-lg">
-                        <ComponentToPrint ref={(el) => (this.componentRef = el)} />
+                        </Link>
+                    </Modal.Footer>
+                </Modal>
+                <div className="page-container-6 container">
+                    <div className="btn-back">
+                        <Link
+                            to={{
+                                pathname: "/u",
+                            }}
+                        >
+                            <BackButton onClick={this.handleShow} />
+                        </Link>
+                    </div>
+                    <p className="subtitle">Your Ikigai, Visualised</p>
+                    <div className="row my-5">
                         <div className="col-lg">
-                            <div className="steps-container container">
-                                <ul>
-                                    {this.state.list.map(item => {
-                                        return (
-                                            <li key={item.id}>
-                                                {item.value}
-                                                <button className="btn-delete" onClick={() => this.deleteItem(item.id)}><Trash /></button>
-                                            </li>
-                                        )
-                                    })}
-                                </ul>
-                                <InputGroup>
-                                    <FormControl
-                                        placeholder="Enter step..."
-                                        aria-label="Enter step..."
-                                        aria-describedby="basic-addon2"
-                                        className="steps-input"
-                                        name="step"
-                                        value={this.state.newItem}
-                                        onChange={e => this.handleInputChange("newItem", e.target.value)}
-                                        onKeyPress={e => e.key === "Enter" && this.addItem()}
-                                    />
-                                    <InputGroup.Append>
-                                        <Button className="btn-add" onClick={() => this.addItem()} disabled={!this.state.newItem}><Add /></Button>
-                                    </InputGroup.Append>
-                                </InputGroup>
+                            <ComponentToPrint ref={(el) => (this.componentRef = el)} />
+                            <div className="col-lg">
+                                <div className="steps-container container">
+                                    <ul>
+                                        {this.state.list.map(item => {
+                                            return (
+                                                <li key={item.id}>
+                                                    {item.value}
+                                                    <button className="btn-delete" onClick={() => this.deleteItem(item.id)}><Trash /></button>
+                                                </li>
+                                            )
+                                        })}
+                                    </ul>
+                                    <InputGroup>
+                                        <FormControl
+                                            placeholder="Enter step..."
+                                            aria-label="Enter step..."
+                                            aria-describedby="basic-addon2"
+                                            className="steps-input"
+                                            name="step"
+                                            value={this.state.newItem}
+                                            onChange={e => this.handleInputChange("newItem", e.target.value)}
+                                            onKeyPress={e => e.key === "Enter" && this.addItem()}
+                                        />
+                                        <InputGroup.Append>
+                                            <Button className="btn-add" onClick={() => this.addItem()} disabled={!this.state.newItem}><Add /></Button>
+                                        </InputGroup.Append>
+                                    </InputGroup>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
-                <ReactToPrint
-                    trigger={() => (
-                    <div className="btn-container-center">
-                        <button type="button" className="btn-default btn-2 btn-lg btn-width-fit">
-                            Export Report
+                    <ReactToPrint
+                        trigger={() => (
+                            <div className="btn-container-center">
+                                <button type="button" className="btn-default btn-2 btn-lg btn-width-fit">
+                                    Export Report
                         </button>
-                    </div>
-                    )}
-                    content={() => this.componentRef}
-                    documentTitle="Venn_PDF"
-                />
+                            </div>
+                        )}
+                        content={() => this.componentRef}
+                        documentTitle="Venn_PDF"
+                    />
 
-                <div class="card card-shadow mt-5">
-                    <div class="card-body">
-                        <div className="share-container container">
-                            <p>Share:</p>
-                            <ul>
-                                <li className="hvr-float"><a href="https://twitter.com/" target="__blank"><Twitter /></a></li>
-                                <li className="hvr-float"><a href="https://facebook.com/" target="__blank"><Facebook /></a></li>
-                                <li className="hvr-float"><a href="https://linkedin.com/" target="__blank"><Linkedin /></a></li>
-                                <li className="hvr-float"><a href="https://whatsapp.com/" target="__blank"><Whatsapp /></a></li>
-                            </ul>
+                    <div class="card card-shadow mt-5">
+                        <div class="card-body">
+                            <div className="share-container container">
+                                <p>Share:</p>
+                                <ul>
+                                    <li className="hvr-float">
+                                        <FacebookShareButton
+                                            url={"https://u.careersocius.com/ikigai"}
+                                            quote={"Find out your ikigai here"}
+                                        >
+                                            <FacebookIcon size={32} round />
+                                        </FacebookShareButton>
+                                    </li>
+                                    <li className="hvr-float">
+                                        <TwitterShareButton
+                                            url={"https://u.careersocius.com/ikigai"}
+                                            quote={"Find out your ikigai here"}
+                                        >
+                                            <TwitterIcon size={32} round />
+                                        </TwitterShareButton>
+                                    </li>
+                                    <li className="hvr-float">
+                                        <LinkedinShareButton
+                                            url={"https://u.careersocius.com/ikigai"}
+                                            quote={"Find out your ikigai here"}
+                                        >
+                                            <LinkedinIcon size={32} round />
+                                        </LinkedinShareButton>
+                                    </li>
+                                    <li className="hvr-float">
+                                        <WhatsappShareButton
+                                            url={"https://u.careersocius.com/ikigai"}
+                                            quote={"Find out your ikigai here"}
+                                        >
+                                            <WhatsappIcon size={32} round />
+                                        </WhatsappShareButton>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div className="container mt-5">
-                    <p>
-                        Achieving Ikigai is a challenging process.<br />
+                    <div className="container mt-5">
+                        <p>
+                            Achieving Ikigai is a challenging process.<br />
                         Your pursuit of Ikigai should draw you closer to a particular cause, skill, or people networks.<br /><br />
 
                         All the best in your pursuit of ikigai!
                     </p>
+                    </div>
                 </div>
-            </div>
-        </>
-      );
+            </>
+        );
     }
-  }
+}
 
 export default Print;
