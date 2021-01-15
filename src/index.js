e5be6f7b830020e2df5b9c5fa0696ffc17585cd2
nkha0010 <nasir.khalid.new@gmail.com>
2021-01-15 11:50:41 +0500

    removed componenttoprint

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import Page3 from "./Page3";
 import Page4 from "./Page4";
 import Page5 from "./Page5";
 import Circa from "./u";
-import Print from "./ComponentToPrint";
 
 const rootElement = document.getElementById("root");
 
@@ -22,7 +21,6 @@ ReactDOM.render(
             <Route path="/lets-find-out-ikigai" component={Page4} />
             <Route path="/export" component={Page5} />
             <Route path="/u" component={Circa} />
-            <Route path="/print" component={Print} />
         </Switch>
     </BrowserRouter>,
     rootElement
