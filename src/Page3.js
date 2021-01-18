431f39228063166f82cd7797ad17e715e34ea197
jllantero <jllantero@gmail.com>
2021-01-18 17:59:33 +0800

    updated page3 layout

--- src/Page3.js
+++ src/Page3.js
@@ -14,37 +14,35 @@ function Page3() {
     return (
         <>  
         <div className="page-container-3">
-            <div className="background-2">
-                <div className="btn-back">
-                    <Link to="/intro">
-                        <BackButton />
+            <div className="btn-back">
+                <Link to="/intro">
+                    <BackButton />
+                </Link>
+            </div>
+            
+            <Logo />
+            
+            <Container className="mt-20 text-center">
+                <h4 className="title-4"><strong>What is ikigai?</strong> </h4>
+                <img src={img3} alt="page-3" className="center my-5" />
+                <p className="p-pg-2">
+                    Ikigai is a Japanese concept to achieving a long and happy life.
+                    <br /> <br />
+                    Ikigai can be described as: 
+                    <br /> <br /> 
+                </p>
+                <p className="p-pg-2">The practice of <strong>
+                    <span style={{ color: '#FFDF00' }}> doing things of value</span>,
+                    <span style={{ color: '#FF5B5B' }}> making progress</span>,
+                    <span style={{ color: '#283972' }}> bringing beauty or utility to others, helping out, and shaping the world around you</span>,
+                    <span style={{ color: '#0EEEAB' }}> even after your ‘official’ professional activity has ended.</span></strong>
+                </p>
+                <div className="btn-container-center">
+                    <Link to="/lets-find-out-ikigai">
+                        <button type="button" class="btn-default btn-2 btn-lg">Next</button>
                     </Link>
                 </div>
-                
-                <Logo />
-                
-                <Container className="mt-20 text-center" fluid="md">
-                    <h4 className="title-4"><strong>What is ikigai?</strong> </h4>
-                    <img src={img3} alt="page-3" className="center my-5" />
-                    <p className="p-pg-2">
-                        Ikigai is a Japanese concept to achieving a long and happy life.
-                        <br /> <br />
-                        Ikigai can be described as: 
-                        <br /> <br /> 
-                    </p>
-                    <p className="p-pg-2">The practice of <strong>
-                        <span style={{ color: '#FFDF00' }}> doing things of value</span>,
-                        <span style={{ color: '#FF5B5B' }}> making progress</span>,
-                        <span style={{ color: '#283972' }}> bringing beauty or utility to others, helping out, and shaping the world around you</span>,
-                        <span style={{ color: '#0EEEAB' }}> even after your ‘official’ professional activity has ended.</span></strong>
-                    </p>
-                    <div className="btn-container-center">
-                        <Link to="/lets-find-out-ikigai">
-                            <button type="button" class="btn-default btn-2 btn-lg">Next</button>
-                        </Link>
-                    </div>
-                </Container>
-            </div>
+            </Container>
         </div>
         </>
     );
